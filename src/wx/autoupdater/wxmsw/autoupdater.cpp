1e8906202ef2cbcf9d4090f00d394d286dac3317
Edênis Freindorfer Azevedo <edenisfa@gmail.com>
2020-03-25 22:12:20 -0300

    Check latest stable version on winsparkle.
    Due to how we generate build version for nightlies, we have something
    like `N.N.N-"commit hash"`. When using winsparkle, the nightly believes
    there is an update, for version `N.N.N`. This is not how it works, so
    we remove the dash and everything after for our update check on
    winsparkle.
    
    It is not clear if this is a bug on winsparkle or if we are abusing
    our definition of version to generate a build from a stable
    version with a bunch of patches applied to it (our WIP for next).

--- src/wx/autoupdater/wxmsw/autoupdater.cpp
+++ src/wx/autoupdater/wxmsw/autoupdater.cpp
@@ -6,6 +6,9 @@
 void initAutoupdater()
 {
     wxString version(vbam_version);
+    size_t index = version.Find("-");
+    if (index != wxString::npos) // even if we are a nightly, only check latest stable version
+        version = version.SubString(0, index - 1);
     win_sparkle_set_appcast_url("http://data.vba-m.com/appcast.xml");
     win_sparkle_set_app_details(L"visualboyadvance-m", L"VisualBoyAdvance-M", version.wc_str());
     win_sparkle_init();
