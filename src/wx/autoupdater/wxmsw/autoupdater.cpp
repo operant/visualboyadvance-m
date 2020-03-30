16a59fbce6029d11a632e3e61a530b2f41cfe6b1
Edênis Freindorfer Azevedo <edenisfa@gmail.com>
2020-03-30 11:59:17 -0300

    Use `str_split` to get related stable version.

--- src/wx/autoupdater/wxmsw/autoupdater.cpp
+++ src/wx/autoupdater/wxmsw/autoupdater.cpp
@@ -1,14 +1,13 @@
 #include "../autoupdater.h"
 #include "../../../common/version_cpp.h"
+#include "../../strutils.h"
 #include "winsparkle-wrapper.h"
 
 
 void initAutoupdater()
 {
-    wxString version(vbam_version);
-    size_t index = version.Find("-");
-    if (index != wxString::npos) // even if we are a nightly, only check latest stable version
-        version = version.SubString(0, index - 1);
+    // even if we are a nightly, only check latest stable version
+    wxString version = str_split(vbam_version, '-')[0];
     win_sparkle_set_appcast_url("http://data.vba-m.com/appcast.xml");
     win_sparkle_set_app_details(L"visualboyadvance-m", L"VisualBoyAdvance-M", version.wc_str());
     win_sparkle_init();
